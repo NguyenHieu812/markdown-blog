f2bfdd41eb5bd01236e8ddca9eacfdb964fd5807
Trong Hieu <hulkgk12082002@gmail.com>
2024-12-04 15:45:57 +0700

    edit project site

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Thần Số Học',
+    description: `Một ứng dụng dự đoán - tra cứu về cuộc đời bạn bằng tên khai sinh và ngày sinh của bạn, giúp bạn hiểu rõ hơn về bản thân và cuộc đời của mình.`,
+    imgSrc: '/static/images/thansohoc.webp',
+    href: 'https://frontend.gnhteam.com/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Gen Bio',
+    description: `Một ứng dụng tạo trang cá nhân và doanh nghiệp để chia sẻ, giới thiệu đến mọi người như một chiếc Card Visit - Business Card, hỗ trợ bạn mở rộng các mối quan hệ cũng như rút ngắn được khoảng cách giữa doanh nghiệp của bạn và đối tác.`,
+    imgSrc: '/static/images/biopage.webp',
+    href: 'https://biopage.gnhteam.com/',
   },
 ]
 
